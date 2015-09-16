d506539c5600ebec98aea86d6ba2c834044739ad
ajaybhatia <prof.ajaybhatia@gmail.com>
2015-09-16 14:59:06 +0530

    First commit

---
+++ src/main/java/com/ajaybhatia/myapplication/SecondActivity.java
@@ -0,0 +1,45 @@
+package com.ajaybhatia.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        result = (TextView) findViewById(R.id.result);
+
+        Intent intent = getIntent();
+        result.setText(intent.getExtras().get("name").toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
